0c244a6454c622330bda0f2705d0ff9eae57d9a9
Chitwan Saharia <sahariac@leto52.iro.umontreal.ca>
2017-08-06 10:26:18 -0400

    cleaned caption processing

--- text-process.py
+++ text-process.py
@@ -8,29 +8,50 @@ from nltk.tokenize import word_tokenize
 import itertools
 from collections import Counter
 import sys
+import tensorflow as tf
+import os
 
-if str(sys.argv[1]) == 'valid':
-	dataType = 'val2014'
-	save_path = '/data/lisatmp4/chitwan/mscoco/caption_processed/processed_captions_valid.pkl'
-elif str(sys.argv[1]) == 'train':
-	dataType = 'train2014'
-	save_path = '/data/lisatmp4/chitwan/mscoco/caption_processed/processed_captions_train.pkl'
-else:
-	print("Bad argument")
-	exit(0)
+vocab_size = 15000
 
-annFile = '/data/lisatmp4/chitwan/mscoco/annotations/captions_%s.json'%(dataType)
+flags = tf.flags
+logging = tf.logging
+
+flags.DEFINE_string("annFile",None,"The annotation file (from mscoco)")
+flags.DEFINE_string("save_path",None,"the save  path for processed dialogues")
+flags.DEFINE_string("make_vocabulary","False","true if you want to make new vocabulary")
+flags.DEFINE_string("vocab_path",None,"path of vocabulary file")
+
+
+FLAGS = flags.FLAGS
+
+assert(FLAGS.annFile)
+assert(FLAGS.save_path)
+assert(FLAGS.vocab_path)
+
+annFile = FLAGS.annFile
+save_path = FLAGS.save_path
+vocab_path = FLAGS.vocab_path
+
 coco = COCO(annFile)
 
 image_ids = coco.getImgIds()
 ann_ids = coco.getAnnIds()
 captions = coco.loadAnns(ann_ids)
 
+if not os.path.exists(save_path):
+	os.makedirs(save_path)
+
+
+if not os.path.exists(vocab_path):
+	os.makedirs(vocab_path)
+
+
+
 def vocabulary():
+	pdb.set_trace()
 	text = [item['caption'].encode('ascii','ignore') for item in captions]
 	tokenised_text = map(lambda text_item :word_tokenize(text_item), text)
 	all_words = list(itertools.chain.from_iterable(tokenised_text))
-	vocab_size  = 15000
 	word_dict = dict(Counter(all_words).most_common(vocab_size))
 	index_to_word = dict(enumerate(word_dict.keys()))
 	word_to_index = {v: k for k, v in index_to_word.items()}
@@ -38,10 +59,14 @@ def vocabulary():
 	word_to_index['STOP_TOKEN'] = word_to_index['.']
 	word_to_index.pop('.',None)
 	word_to_index['START_TOKEN'] = 15001
-	with open('/data/lisatmp4/chitwan/mscoco/caption_processed/vocabulary.pkl','wb') as file:
+	with open(os.path.join(vocab_path,"Vocabulary.pkl"),'wb') as file:
 		cPickle.dump(word_to_index, file)
 
-with open('/data/lisatmp4/chitwan/mscoco/caption_processed/vocabulary.pkl','rb') as file:
+if FLAGS.make_vocabulary == "True":
+	vocabulary()
+
+
+with open(os.path.join(vocab_path,"Vocabulary.pkl"),'rb') as file:
 		word_to_index = cPickle.load(file)
 
 tokenized_index = []
@@ -65,9 +90,9 @@ for _id,item in tokenised_text.items():
 	if count % 5000 == 0:
 		print("Complete Percent " + str(round(count*100/total,3)))
 	count += 1
-	# pdb.set_trace()
+	pdb.set_trace()
 
-with open(save_path,'wb') as file:
+with open(os.path.join(save_path,"Processed_Captions.pkl"),'wb') as file:
 	cPickle.dump(final_dict, file)
 
 
